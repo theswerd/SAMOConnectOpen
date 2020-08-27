ee71bb7f3d5419a9d84074e0afad230f87145fe5
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2020-08-26 20:47:10 -0700

    started building the dropdown page menu thing need to fix it oviously

--- v17/lib/pages/schoolinfo/policiypage.dart
+++ v17/lib/pages/schoolinfo/policiypage.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:v17/components/pageWithHeader.dart';
 
 class Policy extends StatelessWidget {
+  String awnser =
+      "Students must attend class unless excused by an administrator at the request of a parent or teacher, or the student is participating in a school-related activity.  Students are responsible to attend all classes, to be on time, and to come prepared for instruction.";
   @override
   Widget build(BuildContext context) {
-    return PlatformWidget(
-      ios: (c) => CupertinoNavigationBar(
-        middle: Text("Samohi Policy"),
-      ),
-      android: (c) => AppBar(centerTitle: true, title: Text("Samohi Policy")),
+    return PageWithHeader(title: "Policy", body: <Widget>[
+      SliverList(
+          delegate: SliverChildListDelegate([
+        Policies(
+          text: awnser,
+          title: "Attendence",
+        )
+      ]))
+    ]);
+  }
+}
+
+// ignore: must_be_immutable
+class Policies extends StatelessWidget {
+  Policies({@required this.text, @required this.title});
+  String text;
+  String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: DropdownButton(
+          value: 1,
+          items: [
+            DropdownMenuItem(
+              child: Text(title),
+              value: 1,
+            ),
+            DropdownMenuItem(
+              child: Text(text),
+              value: 2,
+            ),
+          ],
+          onChanged: (_) {}),
     );
   }
 }
