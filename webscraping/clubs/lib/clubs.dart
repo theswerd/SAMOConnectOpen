2966293414ca58cb3410333356b351f85e01c9f8
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2020-09-28 15:32:35 -0400

    scraped the website for data on the clubs

--- webscraping/clubs/lib/clubs.dart
+++ webscraping/clubs/lib/clubs.dart
@@ -6,31 +6,40 @@ Future<void> main() async {
   var url = 'https://www.smmusd.org/Page/1155';
   var newdata = [];
   var combine = '';
+  List<Map> newlist = [];
   var response = await http.get(url, headers: {});
   if (response.statusCode == 200) {
     Document document = parse(response.body);
     var clubdata =
         document.getElementsByClassName('ui-article-description')[0].innerHtml;
-    for (var i = 40; i < clubdata.length; i++) {
-      if (clubdata[i] + clubdata[i + 1] + clubdata[i + 2] + clubdata[i + 3] ==
-          '<br>') {
-        if (clubdata[i] +
-                clubdata[i + 1] +
-                clubdata[i + 2] +
-                clubdata[i + 3] +
-                clubdata[i + 5] ==
-            '<br><') {
-          i += 9;
-        } else {
-          for (var j = i - 1; clubdata[j] != '>'; j--) {
-            combine += clubdata[j];
-          }
-          newdata.add(combine.split('').reversed.join(''));
-        }
+    clubdata = clubdata.replaceAll('</p></span></span>', '');
+    var list = clubdata.split('<br><br>');
+    list.removeAt(0);
+    for (var item in list) {
+      if (isWeird(item)) {
+        continue;
       }
-      print(newdata);
+      var mysplititem = item.split('<br>');
+      Map clubmap = new Map();
+      clubmap['name'] = mysplititem[0];
+      clubmap['meetingday'] = mysplititem[1];
+      clubmap['advisor'] = mysplititem[2];
+      clubmap['mission'] = mysplititem[3];
+
+      print(clubmap);
+      print('\n');
     }
   } else {
     print('Response status: ${response.statusCode}');
+  }
+}
+
+bool isWeird(String data) {
+  if (data.startsWith('Wrestling')) {
+    return true;
+  } else if (data.startsWith('Lacrosse')) {
+    return true;
+  } else {
+    return false;
   }
 }
